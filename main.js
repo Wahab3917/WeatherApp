85b16980e1c34a3c926db620f41d13fe5b605efd
Abdul Wahab <wahab3917@gmail.com>
2021-11-13 18:56:24 +0500

    Update main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function showError(error) {
 // Get weather from the API
 
 function getWeather(latitude, longitude) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
   
    fetch(api)
        .then(function(response) {
